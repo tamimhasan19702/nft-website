e9947bbbcc9714b093218122729fb8367fbe5897
tamimhasan19702 <tamimhasancu@gmail.com>
2022-05-26 02:07:31 +0600

    hero section add

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use stict';
+
+const eleToggleFunc = function(elem) {
+  elem.classList.toggle("active");
+}
+
+const navbar = document.querySelector(".navbar");
+const navbarToggleBtn = document.querySelector(".navbar-toggle-btn");
+
+navbarToggleBtn.addEventListener("click", function(){
+  eleToggleFunc(navbar);
+});
