e102b713515ff449273c79fbb34497c67d70ae49
tamimhasan19702 <tamimhasancu@gmail.com>
2022-05-28 02:06:03 +0600

    new product+about section finish

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ const navbarToggleBtn = document.querySelector(".navbar-toggle-btn");
 navbarToggleBtn.addEventListener("click", function(){
   eleToggleFunc(navbar);
 });
+
+// wishlist button
+const wishlistBtn = document.querySelectorAll("[data-wishlist-btn]");
+
+for(let i = 0; i < wishlistBtn.length; i++){
+  wishlistBtn[i].addEventListener("click" , function(){
+   eleToggleFunc(this);
+  });
+}
